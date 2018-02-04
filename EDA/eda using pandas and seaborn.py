7bbb93ee85134c3b1506a73ef3373fc9c8c0d696
Suresh <suresh.studyml@gmail.com>
2018-02-04 07:29:06 +0530

    eda

--- EDA/eda using pandas and seaborn.py
+++ EDA/eda using pandas and seaborn.py
@@ -27,6 +27,7 @@ sns.distplot(titanic_train['Fare'], bins=100, kde=False)
 pd.crosstab(index=titanic_train['Survived'], columns=titanic_train['Sex'])
 pd.crosstab(index=titanic_train['Survived'], columns=titanic_train['Pclass'], margins=True)
 
+# factorplot 
 sns.factorplot(x="Sex", hue="Survived", data=titanic_train, kind="count", size=6)
 sns.factorplot(x="Pclass", hue="Survived", data=titanic_train, kind="count", size=6)
 sns.factorplot(x="Embarked", hue="Survived", data=titanic_train, kind="count", size=6)
@@ -43,7 +44,9 @@ sns.FacetGrid(titanic_train, row="Survived",size=8).map(sns.kdeplot, "Age").add_
 
 
 # Multi variate 
-# FacetGrid creates a grid for rows and columns
+# FacetGrid creates a grid for rows and columns and then analyses the values in map function
+# another option is pair plot   --   sns.pairplot(.....)
+
 sns.FacetGrid(titanic_train, row="Sex", col="Pclass").map(sns.countplot, "Survived")
 sns.FacetGrid(titanic_train, row="Survived", col="Sex").map(sns.distplot, "Fare")
 sns.FacetGrid(titanic_train, row="Survived", col="Sex").map(sns.distplot, "Age")
